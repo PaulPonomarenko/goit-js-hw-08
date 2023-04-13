a7b2d3ca9e50071eedf67cdadf49fe063aa43a99
PaulPonomarenko <pashaponomarenkoa1999@gmail.com>
2023-04-13 04:35:26 +0300

    добавляет первое задание

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,28 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+const mainGalleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGallery(galleryItems);
+
+function createGallery(image) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`;
+    })
+    .join('');
+}
+mainGalleryContainer.insertAdjacentHTML('afterbegin', galleryMarkup);
+
+const lightbox = new SimpleLightbox('.gallery__link', {
+  captionSelector: 'img',
+  captionsData: 'alt',
+  captionDelay: 250,
+  enableKeyboard: true,
+});
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
