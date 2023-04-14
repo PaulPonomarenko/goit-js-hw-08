1e1f05cc5e1d5d86cf1e4c05abdf712d50d6d13f
PaulPonomarenko <pashaponomarenkoa1999@gmail.com>
2023-04-14 04:44:27 +0300

    добавляет  второе дз

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,11 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const currentTime = 'videoplayer-current-time';
+function setCurrenTime(data) {
+  localStorage.setItem(currentTime, data.seconds);
+}
+player.on('timeupdate', throttle(setCurrenTime, 1000));
+player.setCurrentTime(localStorage.getItem(currentTime));
